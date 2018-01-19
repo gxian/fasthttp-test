d6addab27b452a5f5c5d5dcd99e27573dbb1c84c
gaoxiang <gaoxiang.ian@gmail.com>
2018-01-19 14:38:00 +0800

    去掉多余操作

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/valyala/fasthttp"
@@ -27,16 +26,16 @@ func main() {
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+	// fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	// fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	// fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	// fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	// fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	// fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	// fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	// fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	// fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	// fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
 	ctx.SetContentType("application/json")
 	ctx.SetBody(ctx.PostBody())
